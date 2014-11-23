0f0be1dde6ebc9c69611ab99b0f0c9a046c5ed59
t3knomanser@gmail.com <remyporter@jetpackshark.com>
2014-11-22 19:17:56 -0500

    Updated data, improved script.

--- twit.py
+++ twit.py
@@ -5,18 +5,20 @@ import time
 
 hashtag = " #GamerGate"
 
-twit_freq = 62
+twit_freq = 30
 
 def next_tweet():
     res = ""
     while len(res) + len(hashtag) < 125:
         res += rambler.get_string()
     if len(res) + len(hashtag) > 140:
-        return (True, res[:140-len(hashtag)] + hashtag)
+        res = res[:(140-len(hashtag))]
+        res = res[:res.rindex(" ")]
+        return (True, res + hashtag)
     return (False, res + hashtag)
 
 def post_next(twitter, depth=0):
-    if depth > 3: return
+    if depth > 2: return
     nxt = next_tweet()
     twitter.statuses.update(status=nxt[1])
     if nxt[0]:
@@ -32,8 +34,8 @@ def main():
             creds["consumersecret"]
         ))
     while(True):
-        time.sleep(twit_freq*60)
         post_next(twit)
+        time.sleep(twit_freq*60)
 
 if __name__ == "__main__":
     main()
